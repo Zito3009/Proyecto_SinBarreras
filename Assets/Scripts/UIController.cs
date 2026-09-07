afa6f6c3c2c7db4712ae258a06c0edac3914b940
Luciacaballero03 <lulicaba03@gmail.com>
2026-09-07 14:26:17 -0300

    ui en el nivel real pero no anda

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject panelInicio;
+    public GameObject panelLogin;
+    public GameObject panelRegistro;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void AbrirLogin()
+    {
+        panelInicio.SetActive(false);
+        panelLogin.SetActive(true);
+    }
+
+    public void AbrirRegistro()
+    {
+        panelInicio.SetActive(false);
+        panelRegistro.SetActive(true);
+    }
+
+    public void VolverAlInicio()
+    {
+        panelInicio.SetActive(true);
+        panelLogin.SetActive(false);
+        panelRegistro.SetActive(false);
+    }
+    public void EmpezarJuego()
+    {
+        panelInicio.SetActive(false);
+        panelLogin.SetActive(false);
+        panelRegistro.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1f;
+    }
+}
